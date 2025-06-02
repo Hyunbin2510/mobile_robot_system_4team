bb7ca41c6392907117d0c8a2de32777c548f2292
hyeonjeong <guswjdd2935@gmail.com>
2025-06-02 11:07:36 +0900

    added red, yellow color detection motion

--- main.py
+++ main.py
@@ -23,40 +23,101 @@ lsa = LSA(Port.S1, 0X14)
 
 
 # Write your program here.
+
+########Function#########
+
+# Start function
 def start():
-    '''
-    steering_motor Calibration
-    '''
     steering_motor.run(100)
-    while 1:
-        a = steering_motor.angle()
+    while True:
+        a=steering_motor.angle()
         wait(100)
-        b = steering_motor.angle()
-
-        if a == b:
+        b=steering_motor.angle()
+        if a==b:
             break
-
+        
     steering_motor.stop()
-    steering_motor.run_angle(-50, 100)
+    steering_motor.run_angle(-100,100)
     steering_motor.reset_angle(0)
     
-def steering_max(switch):
-    '''
-    왼쪽으로 쭉 조향을 꺾음
-    switch = 1 : left
-    switch = -1 : right
-    '''
-    steering_motor.run(switch * 100)
-    while 1:
-        a = steering_motor.angle()
-        wait(100)
-        b = steering_motor.angle()
+#Color sensor control function
+def color_detection():
+    r, g, b = color_sensor.rgb()
 
-        if a == b:
-            break
+    if 50 <= r <= 100 and 0 <= g <= 40 and 0 <= b <= 40:
+        return "red"
+    elif 50 <= r <= 100 and 50 <= g <= 100 and 0 <= b <= 40:
+        return "yellow"
+    return None
 
-    steering_motor.stop()
+def red_detection_motion():
+    # 레드 3번 감지 후 수행할 동작 구현
+    ev3.speaker.say("Red detected 3 times")
+    # 여기에 추가 모션이나 신호등 감지 알고리즘 넣으면 됩니다
 
-if __name__ == '__main__':
-    start()
-    while 
+def yellow_detection_motion():
+    global yellow_stopped
+    if not yellow_stopped:
+        run_motor.stop()
+        steering_motor.stop()
+        ev3.light.on(Color.RED)
+        wait(1000)
+        ev3.light.on(Color.YELLOW)
+        wait(1000)
+        ev3.light.on(Color.GREEN)
+        wait(1000)
+        run_motor.run(150)
+        yellow_stopped = True
+
+def reset_yellow_flag():
+    global yellow_stopped
+    yellow_stopped = False
+
+
+####################
+    
+########main########
+
+start()
+run_motor.run(150)
+
+th = 50
+Gain = 3
+red_count=0
+previous_color = None
+yellow_stopped = False
+
+
+while True:
+    try:
+        detected_color = color_detection()
+        
+        if detected_color == "red" and previous_color != "red":
+            red_count += 1
+            if red_count == 4:
+                red_detection_motion() 
+            elif red_count == 5:
+                run_motor.stop()
+                steering_motor.stop()
+            
+        elif detected_color == "yellow":
+            yellow_detection_motion()
+
+        else:
+            reset_yellow_flag()
+        
+        previous_color = detected_color
+
+        if not yellow_stopped:
+            data = lsa.ReadRaw_Calibrated()
+            sensor_value = list(data)
+            line_value = sensor_value[3]
+
+            error = th - line_value
+            correction = Gain * error
+            correction = max(min(correction, 90), -90)
+
+            steering_motor.run_target(1500, correction)
+
+    except:
+        pass
